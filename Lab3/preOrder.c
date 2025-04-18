8e3031e844ac1b26427a2b9fb2992ca97eb8bfe3
Troy Dixon <troydixon0608@gmail.com>
2025-04-17 22:04:03 -0500

    Add files via upload

---
+++ Lab3/preOrder.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  struct TreeNode *left;
+  struct TreeNode *right;
+};
+
+// Note: The returned array must be malloced, assume caller calls free().
+void inOrder(struct TreeNode *root, int *arr, int *returnSize) {
+  if (root != NULL) {
+    arr[(*returnSize)++] = root->val;
+    inOrder(root->left, arr, returnSize);
+    inOrder(root->right, arr, returnSize);
+  }
+}
+int *inorderTraversal(struct TreeNode *root, int *returnSize) {
+  int *temp = (int *)malloc(100 * sizeof(int));
+  int x = 0;
+  *returnSize = x;
+  inOrder(root, temp, returnSize);
+  return temp;
+}
